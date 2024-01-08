81645fc0d7bbbe385ea256c08d98565a7efd9eb3
JeanAlves00 <154983968+JeanAlves00@users.noreply.github.com>
2024-01-08 10:10:15 -0400

    Adicionado eventos a elementos HTML

--- main.js
+++ main.js
@@ -2,15 +2,33 @@ const form = document.getElementById('form-registro');
 
 function validaNome(nomeCompleto) {
     const nomeComoArray = nomeCompleto.split(' ');
-    return nomeComoArray.length > 2;
+    return nomeComoArray.length >= 2;
 }
 
-form.addEventListener('submit', function(){
+form.addEventListener('submit', function(e) {
+    let formEValidao = false;
     e.preventDefault();
 
-    const nomeRegistro = document.getElementById('nome-registro');
-    if (!validaNome(nomeRegistro.value)) {
-        alert('Nome não esta completo');
+    const nome = document.getElementById('nome');
+    const senha = document.getElementById('password');
+    const email = document.getElementById('email');
+    const telefone = document.getElementById('telefone');
+    const datanascimento = document.getElementById('data-nascimento');
+    const mensagemSuceso = 'registro efetuado com sucesso';
+
+
+    formEValidao = validaNome(nome.value)
+    if (formEValidao) {
+        alert(mensagemSuceso);
+
+        nome.value = '';
+        password.value = '';
+        email.value = '';
+        telefone.value = '';
+        datanascimento.value = '';
+
+    } else {
+        alert('Nome não está completo');
     }
 })
 
